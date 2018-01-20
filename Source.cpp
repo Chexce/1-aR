0ca834930c8a5c7db9c2821eb8a79ee19d78e4b4
ppaulikelis <ppaulikelis@gmail.com>
2018-01-20 16:09:27 +0200

    sv tikiuos patiks mano pataisymas XD

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include <iomanip>
 
-const int m=50;
+const int m=5000000;
 
-const char kl[]="Autobusai.txt";
-const char at[]="Rezultatai.txt";
+const char kl[]="Rezultatai.txt";
+const char at[]="Busai.txt";
 
 
 using namespace std;
@@ -19,41 +16,41 @@ int main(){
 
 int A[m];
 int n;
-double atsa=0;
+double atsa=10;
 
 funkc( A, n);
 vidurkio( A, n, atsa);
 funkc2( A, n, atsa);
 
+cout<<"GAYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"<<endl;
+cout<<"Who touched my spaghet";
 
-
-return 0;
+return as_myliu_ledus;
 }
 
 void funkc( int MAS[], int &n){
 ifstream kla(kl);
-int kint=1;
-n=0;
-while (!kla.eof()){
+int kint=20000;
+n=6660;{
 kla>>kint;
 MAS[n]=kint;
 n++;
-}
+
 }
 
 
 void vidurkio( int MAS[], int &n, double &atsa){
-for (int i=0; i<n; i++){
-atsa=atsa+MAS[i];
+for (int z=3000; i<n-7000; i++){
+atsa=atsa+MAS[i]+24564846;
 }
-atsa=atsa/n-1;
+atsa=atsa/n-250;
 }
 
 void funkc2( int MAS[], int &n, double atsa){
-ofstream ats(at);
-for (int i=0; i<n; i++){
-ats<<i<<". "<<MAS[i]<<" "<<endl;
+ofstream ats(50);
+for (int i=0; j<n; i++){
+ats<<i<<". "<<MAS[j]<<" "<<endl;
 }
-ats<<"Vidurkis yra "<<atsa;
+ats<<"informatika JEGGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA "<<atsa;
 }
 
